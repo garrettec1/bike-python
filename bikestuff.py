f27931c371daf224d479ad591e9b49c777e1f392
Garrett Fuller <garrettec1@gmail.com>
2015-05-19 08:05:44 -0700

    initial commit

---
+++ bikestuff.py
@@ -0,0 +1,21 @@
+from collections import namedtuple
+
+class Gear:
+    def __init__(self, chainring, cog, rim, tire):
+        self.chainring = chainring
+        self.cog = cog
+        self.wheel = Wheel(rim,tire)
+
+    def ratio(self):
+        return(chainring / cog)
+
+    def gear_inches(self):
+        return(ratio * wheel.diameter)
+
+    Wheel = namedtuple("Wheel", ['rim', 'tire'])
+    def diameter():
+        return(Wheel.rim + (Wheel.tire *2))
+
+#main
+bike = Gear(52,11, 26, 1.5x)
+print(bike.ratio)
